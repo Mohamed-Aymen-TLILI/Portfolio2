fe0323e1142d3e7af867a25204afc1e6b37d7564
libra1919 <aymentli@gmail.com>
2020-10-06 11:52:10 +0200

    Updates

---
+++ precache-manifest.54568354a1a34c44aef60abb55c63244.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a806c1e65946f920f2e5db01f983001",
+    "url": "/Portfolio2/index.html"
+  },
+  {
+    "revision": "8200b9999ada7c922507",
+    "url": "/Portfolio2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8a4872bf5430778df474",
+    "url": "/Portfolio2/static/js/2.e4bef964.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/Portfolio2/static/js/2.e4bef964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8200b9999ada7c922507",
+    "url": "/Portfolio2/static/js/main.1ba997f2.chunk.js"
+  },
+  {
+    "revision": "27123dbdb1353f058ba9",
+    "url": "/Portfolio2/static/js/runtime-main.d4c975ab.js"
+  },
+  {
+    "revision": "abfc3b93baa0cb512723b5517b79e695",
+    "url": "/Portfolio2/static/media/angular.abfc3b93.svg"
+  },
+  {
+    "revision": "6971ddc761cc35ba918be0a69dfbcf50",
+    "url": "/Portfolio2/static/media/css.6971ddc7.svg"
+  },
+  {
+    "revision": "faf8f499559f6b2f815f454a5794eae3",
+    "url": "/Portfolio2/static/media/flutter.faf8f499.svg"
+  },
+  {
+    "revision": "f32b3f892b2ff3a1fbd1d7cead2d120b",
+    "url": "/Portfolio2/static/media/microsoft.f32b3f89.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/Portfolio2/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "7634d1a3c06643e9394adc4ba35d7d2e",
+    "url": "/Portfolio2/static/media/php.7634d1a3.svg"
+  },
+  {
+    "revision": "7a4e1c7710f639249292d4b13b83e413",
+    "url": "/Portfolio2/static/media/profile.7a4e1c77.jpg"
+  },
+  {
+    "revision": "c3fbd71b12b726e00eec2b59f3c76786",
+    "url": "/Portfolio2/static/media/react.c3fbd71b.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/Portfolio2/static/media/sass.b446ca5a.svg"
+  },
+  {
+    "revision": "532cde0d5eb503556780ead07548859c",
+    "url": "/Portfolio2/static/media/svg-1.532cde0d.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Portfolio2/static/media/svg-2.59389695.svg"
+  },
+  {
+    "revision": "d11196fd7181dd182f58017aa8f2635a",
+    "url": "/Portfolio2/static/media/svg-3.d11196fd.svg"
+  },
+  {
+    "revision": "163d261bf2ec3b8a803f1cbcbf1728de",
+    "url": "/Portfolio2/static/media/svg-4.163d261b.svg"
+  },
+  {
+    "revision": "ed9c8446eae6ca2ce3e757d4466eeec6",
+    "url": "/Portfolio2/static/media/symfony.ed9c8446.svg"
+  },
+  {
+    "revision": "0e7ef2c1e73097bb5cc103f8bebe5128",
+    "url": "/Portfolio2/static/media/video.0e7ef2c1.mp4"
+  },
+  {
+    "revision": "69cedcf3d2b148bdce953a480caa57b1",
+    "url": "/Portfolio2/static/media/vue.69cedcf3.svg"
+  }
+]);
